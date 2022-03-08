9e545dfe3664d4f4edf9194df13abd31192c0217
parasa-r <saymer98@gmail.com>
2022-03-08 10:14:59 +0100

    test

--- android/src/main/java/ch/byrds/capacitor/contacts/Contacts.java
+++ android/src/main/java/ch/byrds/capacitor/contacts/Contacts.java
@@ -130,7 +130,18 @@ public class Contacts extends Plugin {
                     String displayName = contactsCursor.getString(contactsCursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
                     jsContact.put(DISPLAY_NAME, displayName);
 
-                   String whereName = ContactsContract.Data.MIMETYPE + " = ? AND " + ContactsContract.CommonDataKinds.StructuredName.CONTACT_ID + " = ?";
+                    String given = "";
+                    String family = "";
+                    if(displayName.split("\\w+").length > 1){
+                        family = displayName.substring(displayName.lastIndexOf(" ") + 1);
+                        given = displayName.substring(0, displayName.lastIndexOf(' '));
+                    } else {
+                        given = displayName;
+                    }
+                    jsContact.put(GIVEN_NAME, given);
+                    jsContact.put(FAMILY_NAME, family);
+
+                  /* String whereName = ContactsContract.Data.MIMETYPE + " = ? AND " + ContactsContract.CommonDataKinds.StructuredName.CONTACT_ID + " = ?";
                     String[] whereNameParams = new String[] { ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE, contactId };
                     Cursor nameCur = contentResolver.query(ContactsContract.Data.CONTENT_URI, null, whereName, whereNameParams, ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME);
                     while (nameCur.moveToNext()) {
@@ -145,7 +156,7 @@ public class Contacts extends Plugin {
                     }
                     nameCur.close();
 
-                   /* String given = contactsCursor.getString(contactsCursor.getColumnIndex(ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME));
+                    String given = contactsCursor.getString(contactsCursor.getColumnIndex(ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME));
                     String family = contactsCursor.getString(contactsCursor.getColumnIndex(ContactsContract.CommonDataKinds.StructuredName.FAMILY_NAME));
                     if(given != null && given.length() > 0){
                         Log.d("GIVEN", given);
@@ -154,7 +165,7 @@ public class Contacts extends Plugin {
                     if(family != null && family.length() > 0) {
                         Log.d("FAMILY", family);
                         jsContact.put(FAMILY_NAME, family);
-                    }*/
+                    } */
 
                     JSArray jsPhoneNumbers = new JSArray();
                     jsContact.put(PHONE_NUMBERS, jsPhoneNumbers);
